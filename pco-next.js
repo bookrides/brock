8d2c52dbbdacea486daa25d50824d48548520bb8
bookrides <john.moon@bookridesonline.com>
2021-03-31 16:47:43 -0500

    Add files via upload

---
+++ pco-next.js
@@ -0,0 +1,54 @@
+(function () {
+    var getData = function(url, callback, auth){
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", url);
+        xhr.setRequestHeader("Authorization", `Basic ${auth}`);
+
+        xhr.onreadystatechange = function () {
+            if(xhr.readyState === 4) {
+                callback(xhr.responseText)
+            }
+        }
+        xhr.send();
+    }
+
+    var tag_id = document.currentScript.getAttribute('tag-id')
+    var auth = document.currentScript.getAttribute("auth")
+    if(!tag_id || !auth) {
+        console.error('No tag_id or auth passed. Cannot load next event')
+        return
+    }
+
+    var id = `next-event-${tag_id}`
+    var div = document.createElement("div")
+    div.id = id
+    document.currentScript.insertAdjacentElement('afterEnd', div)
+
+    var eventUrl = "https://api.planningcenteronline.com/resources/v2/event_instances?order=starts_at&include=event_times&filter=future&where[tag_ids]=" + tag_id;
+    
+    getData(eventUrl, function(rawData) {
+        var data = JSON.parse(rawData).data
+        var nextEvent = data[0]
+
+        if(nextEvent) {
+            var d = new Date(nextEvent.attributes.starts_at)
+            var end = new Date(nextEvent.attributes.ends_at)
+
+            var dateFormat = {weekday: 'long', day : 'numeric', month : 'short', year : 'numeric'}
+            var nextEventDate = d.toLocaleDateString('en-US', dateFormat)
+            var nextEventTime = d.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+            var nextEventEndTime = end.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+
+            var elem = `<div style="text-align:center;margin:auto;width:100%;padding:0;">
+                <a target="_blank" style="text-decoration: none;font-family: arial;color: white;background-color: black;padding: 1rem;font-size: 12px; text-align: center;"
+                    href="https://bethelsrock.churchcenter.com/calendar/event/${nextEvent.id}">${nextEventDate} ${nextEventTime} - ${nextEventEndTime}</a>
+            </div>`
+
+            var nextEventContainer = document.getElementById(id)
+            nextEventContainer.innerHTML = elem
+        }
+        else {
+            console.error('No next event for tag_id passed.')
+        }
+    }, auth)
+}())
